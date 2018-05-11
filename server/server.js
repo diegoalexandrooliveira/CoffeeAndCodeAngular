9e208ea73aed7be626d8c1b99c9ec2cf1c42ae5a
Diego Alexandro de Oliveira <diego.alexandro@totvs.com.br>
2018-05-11 13:49:11 -0300

    .

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require("express")();
 const cors = require("cors");
 
-express.use(cors({ allowedHeaders: "*", methods: "*", origin: "*" }));
-express.get("/api/imagens", (req, res) => {
-  let imagens = require("./dados");
-  return res.json(imagens);
-});
+express.use(cors({
+  allowedHeaders: "*",
+  methods: "*",
+  origin: "*"
+}));
+
+express.get("/api/imagens", (req, res) =>
+  res.json(require("./dados"))
+);
 
 express.listen(8080, "localhost", () => {
   console.log("Servidor rodando.");
